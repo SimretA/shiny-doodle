7a576525e79347952e6ec61c0f8173d1d0fb478f
SimretA <simretaraya7@gmail.com>
2020-04-09 19:04:26 +0300

    bugsss

--- src/components/review-listing/review-strip.component.js
+++ src/components/review-listing/review-strip.component.js
@@ -34,8 +34,8 @@ export default function ReviewStrip(props) {
             <p style={{paddingLeft:"5"}}>{firstName} {lastName}</p>
         </InlineWrapper>
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            {/*<p>{props.data.content}</p>*/}
+            {/*<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>*/}
+            <p>{props.data.content}</p>
 
 
     </Wrapper>;
